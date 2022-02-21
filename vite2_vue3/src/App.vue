b4d1d73225c065b7baca7cb2ed7facb3e28d2466
wumo1016 <760478279@qq.com>
2022-02-21 16:02:34 +0800

    init

--- vite2_vue3/src/App.vue
+++ vite2_vue3/src/App.vue
@@ -1,10 +1,9 @@
-<script setup>
-
+<script setup lang="ts">
+import { RouterView } from "vue-router";
 </script>
 
 <template>
-  vue3
+  <RouterView />
 </template>
 
-<style>
-</style>
+<style></style>
---
+++ vite2_vue3/src/views/Home.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>home</main>
+</template>
